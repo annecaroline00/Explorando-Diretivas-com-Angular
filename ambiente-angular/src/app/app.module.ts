6e204abaa190e3d15803f6df7d8c75c1346d9144
anne <annecarolinedss00@gmail.com>
2022-08-14 11:02:20 -0300

    main

--- ambiente-angular/src/app/app.module.ts
+++ ambiente-angular/src/app/app.module.ts
@@ -2,30 +2,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './directives/marca-texto.directive';
+import { PeopleService } from './shared/people.service';
+
 import { ListPeopleComponent } from './shared/component/list-people/list-people.component';
 import { ListApiComponent } from './components/list-api/list-api.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarcaTextoDirective,
-    ListPeopleComponent,
-    ListApiComponent
+    MarcaTextoDirective, 
+    ListApiComponent,
+    ListPeopleComponent
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule,
     
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
