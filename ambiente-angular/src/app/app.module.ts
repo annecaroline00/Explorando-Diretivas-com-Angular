9af2e7c7ba59294b775eba48b6eee7d72c4c8656
anne <annecarolinedss00@gmail.com>
2022-08-14 00:45:53 -0300

    commit

--- ambiente-angular/src/app/app.module.ts
+++ ambiente-angular/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './directives/marca-texto.directive';
+import { ListPeopleComponent } from './shared/component/list-people/list-people.component';
+import { ListApiComponent } from './components/list-api/list-api.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MarcaTextoDirective
+    MarcaTextoDirective,
+    ListPeopleComponent,
+    ListApiComponent
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgModule,
-    MarcaTextoDirective, 
+    CommonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
